8cf706a639d41abeddabf224e489db1a11b21c46
Lukas Matta <lukas@matta.sk>
2020-03-10 18:47:34 +0100

    Add table view

---
+++ Homework2/FirstRowTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FirstRowTableViewCell.swift
+//  Homework2
+//
+//  Created by Lukáš Matta on 10/03/2020.
+//  Copyright © 2020 Lukáš Matta. All rights reserved.
+//
+
+import UIKit
+
+class FirstRowTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
